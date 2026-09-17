dcd8ba0412887baf8e12d6592683cdbf4a46e21c
hazel0209 <karisk0209@gmail.com>
2026-09-17 18:13:14 +0900

    bm

--- js/custom_gsap.js
+++ js/custom_gsap.js
@@ -36,41 +36,51 @@ tl.to(".circ", {
     ease: "none",
   });
 
-const ring = document.querySelector(".time_cir");
-gsap.set(ring, { "--p": 0, "--p2": 0 });
-gsap.set(".con03 ul li", { opacity: 0, y: 30 });
+// const ring = document.querySelector(".time_cir");
+// gsap.set(ring, { "--p": 0, "--p2": 0 });
+// gsap.set(".con03 ul li", { opacity: 0, y: 30 });
 
-const tl2 = gsap.timeline({
-  // ← tl과 이름 겹치지 않게 tl2로
-  scrollTrigger: {
-    trigger: ".con03",
-    start: "top top",
-    end: "+=400%",
-    scrub: 1,
-    pin: true,
-  },
-});
-
-tl2
-  .to(ring, { "--p": 100, duration: 4, ease: "none" })
-  .to(".history_25 li:nth-child(1)", { opacity: 1, y: 0, duration: 1 }, 0.3)
-  .to(".history_25 li:nth-child(2)", { opacity: 1, y: 0, duration: 1 }, 1.5)
-  .to(".history_25 li:nth-child(3)", { opacity: 1, y: 0, duration: 1 }, 2.6)
-  .to(".history_25 li:nth-child(4)", { opacity: 1, y: 0, duration: 1 }, 3.6)
-  .to(".history_25 li", { opacity: 0, y: 30, duration: 1 }, 4.2)
-  .call(
-    () => {
-      ring.textContent = tl2.scrollTrigger.direction === 1 ? "2026" : "2025";
+gsap
+  .timeline({
+    // ← tl과 이름 겹치지 않게 tl2로
+    scrollTrigger: {
+      trigger: ".con03",
+      start: "top top",
+      end: "+=400%",
+      scrub: 1,
+      pin: true,
     },
-    null,
-    4.6,
-  )
-  .to(ring, { "--p": 100, duration: 4, ease: "none" }, 5.1)
-  .to(ring, { "--p2": 100, duration: 4, ease: "none" }, 5.1)
-  .to(".history_26 li:nth-child(1)", { opacity: 1, y: 0, duration: 1 }, 5.4)
-  .to(".history_26 li:nth-child(2)", { opacity: 1, y: 0, duration: 1 }, 6.6)
-  .to(".history_26 li:nth-child(3)", { opacity: 1, y: 0, duration: 1 }, 7.7)
-  .to(".history_26 li:nth-child(4)", { opacity: 1, y: 0, duration: 1 }, 8.7);
+  })
+  .from(".con03 .history_25 li ", {
+    y: -50,
+    opacity: 0,
+    stagger: 0.2,
+  })
+  .from(".con03 .history_26 li", {
+    y: 50,
+    opacity: 0,
+    stagger: 0.2,
+  });
+// tl2
+//   .to(ring, { "--p": 100, duration: 4, ease: "none" })
+//   .to(".history_25 li:nth-child(1)", { opacity: 1, y: 0, duration: 1 }, 0.3)
+//   .to(".history_25 li:nth-child(2)", { opacity: 1, y: 0, duration: 1 }, 1.5)
+//   .to(".history_25 li:nth-child(3)", { opacity: 1, y: 0, duration: 1 }, 2.6)
+//   .to(".history_25 li:nth-child(4)", { opacity: 1, y: 0, duration: 1 }, 3.6)
+//   .to(".history_25 li", { opacity: 0, y: 30, duration: 1 }, 4.2)
+//   .call(
+//     () => {
+//       ring.textContent = tl2.scrollTrigger.direction === 1 ? "2026" : "2025";
+//     },
+//     null,
+//     4.6,
+//   )
+//   .to(ring, { "--p": 100, duration: 4, ease: "none" }, 5.1)
+//   .to(ring, { "--p2": 100, duration: 4, ease: "none" }, 5.1)
+//   .to(".history_26 li:nth-child(1)", { opacity: 1, y: 0, duration: 1 }, 5.4)
+//   .to(".history_26 li:nth-child(2)", { opacity: 1, y: 0, duration: 1 }, 6.6)
+//   .to(".history_26 li:nth-child(3)", { opacity: 1, y: 0, duration: 1 }, 7.7)
+//   .to(".history_26 li:nth-child(4)", { opacity: 1, y: 0, duration: 1 }, 8.7);
 
 (function createCircleBackground() {
   const container = document.querySelector(".con03");
